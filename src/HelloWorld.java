be89064521d8901485d27008eda4d3f9423d7c5e
John Pierce <pierce.johndaniel@gmail.com>
2022-09-19 15:30:18 -0500

    int variables

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,8 +1,11 @@
 public class HelloWorld {
 
     public static void main(String[] args){
-        System.out.println("Hello World!");
-
+        //System.out.println("Hello World!");
+        int myAge = 36;
+        myAge = 37;
+        int herAge = myAge;
+        System.out.println(herAge);
 
     }
 }
